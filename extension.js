fbc9454220bd9ec265e98aad1b0d42ababf08e8d
benjaminng <shen_soonng@hotmail.com>
2020-05-24 19:53:38 +0800

    change command name and allow suffix config

--- extension.js
+++ extension.js
@@ -2,9 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-const fs = require("fs");
-const path = require("path");
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -15,17 +12,18 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test-switcher-vs" is now active!');
+	console.log('Congratulations, your extension "Test Switcher" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-switcher-vs.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('test-switcher-switch-code', function () {
 		// The code you place here will be executed every time your command is executed
 
 		//console.log(vscode.window.activeTextEditor.document.uri.fsPath);
 
-		const fileSuffixes = ['.unit.test'];
+		const fileSuffixes = vscode.workspace.getConfiguration('test-switcher')
+			.get('test.suffix', ['.unit.test', '.int.test', '.int.narrow.test']);
 
 		const currentOpenFileName = vscode.window.activeTextEditor.document.uri.fsPath.replace(vscode.workspace.rootPath, '');
 
